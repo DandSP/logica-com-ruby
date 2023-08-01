f58a3a8ecd064c99f8deaabbb9c636b4591ff0e9
Danúbio <danubio.sousapereira@gmail.com>
2023-08-01 20:59:38 -0300

    corrigindo erro de português

--- Advanced/tradutor/app.rb
+++ Advanced/tradutor/app.rb
@@ -3,7 +3,7 @@ require_relative 'user'
 esp = 60
 puts "Digite o texto que deseja traduzir: ".center(esp)
 text = gets.strip
-puts "Idioma do texto: (Digite o idioma em inglês(Ou a siglag))".center(esp)
+puts "Idioma do texto: (Digite o idioma em inglês(Ou a sigla))".center(esp)
 org = gets.strip
 puts "Para que idioma deseja traduzir? (Digite a sigla ou em inglês)"
 lang = gets.strip
